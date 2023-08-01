46f110f6ef0a2de96c8b6f0f6bf6bc06142805dc
vamsi0874 <126262877+vamsi0874@users.noreply.github.com>
2023-08-01 22:00:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
   $('document').ready(()=>{
 
     caseRegistration.addEventListener('click',()=>{
-    $('.content').load('html/reg.html')
+    $('.content').load('reg.html')
     })
 
     about.addEventListener('click',()=>{
